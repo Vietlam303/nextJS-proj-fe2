3ab6472928c2bfefbeaa8cfe61a7ee17650edcff
phamduythe <thepham3603@gmail.com>
2024-05-21 19:59:41 +0700

    push update

---
+++ src/app/page.jsx
@@ -0,0 +1,6 @@
+const Home = () => {
+  // throw new Error('error is home')
+  return <div>Hello World!</div>;
+};
+
+export default Home;
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import styles from "./links.module.css"
+import NavLink from "./navLink/navLink"
+const Links = () => {
+    
+    const links = [
+        {
+            title: "Home",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+        {
+            title: "Blog",
+            path: "/blog",
+        }
+    ];
+    const session = true;
+    const isAdmin = false;
+    return (
+        <div className={styles.links}>
+            {links.map((link => (
+                <NavLink item = {link} key={link.title}/>
+            )))}{
+                session ? (
+                   <>
+                     {isAdmin && <NavLink item={{title:"Admin",path:"/admin"}}/>}
+                    <button className={styles.btnlogout}>Log Out</button>
+                   </>
+                ) : (
+                    <NavLink item ={{title:"Login", path:"/login"}}/>
+
+                )}
+
+
+        </div>
+    );
+};
+
+export default Links;
