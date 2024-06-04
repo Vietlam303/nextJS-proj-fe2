6b3229db5bfef23b0e694a1570427dbe8d289b62
linhle41 <lebachlinh0347@gmail.com>
2024-06-05 01:03:09 +0700

    linhupdate

--- FE/app/author/page.jsx
+++ FE/app/author/page.jsx
@@ -2,146 +2,162 @@ import style from "./author.module.css"
 import Image from "next/image";
 import Link from "next/link";
 import { FaAngleRight } from "react-icons/fa6";
+import { Row, Col } from 'react-bootstrap';
 
 const authorPage = () => {
 
    return (
 
-      <div className={style.container}>
-         <h1>Author</h1>
-         <div className={style.indexing}>
-            <Link href="/" > Home</Link> <FaAngleRight fontSize={10} /> Author
-         </div>
-         <div className={style.container1}>
-            <div>
-               <div className={style.content}>
+      <div>
+         <div className={style.container}>
+            <h1>Author</h1>
+            <div className={style.indexing}>
+               <Link href="/" > Home</Link> <FaAngleRight fontSize={10} /> Author
+            </div>
+            <div className={style.container1}>
+               <div>
+                  <div className={style.content}>
+                  </div>
+               </div>
+               <div>
+                  <div className={style.content}>
+                  </div>
+               </div>
+               <div>
+                  <div className={style.content}>
+                  </div>
+               </div>
+               <div>
+                  <div className={style.content}>
+                  </div>
+               </div>
+               <div>
+                  <div className={style.content}>
+                  </div>
+               </div>
+               <div>
+                  <div className={style.content}>
+                  </div>
                </div>
             </div>
             <div>
-               <div className={style.content}>
+               <div className={style.filter}>
+                  <select>
+                     <option value="">Select an author</option>
+                     <option value="1">Tố Hữu</option>
+                     <option value="2">Gegor Canegi</option>
+                     <option value="3">Ông Thọ</option>
+                     <option value="4">Napoleon Hill</option>
+                  </select>
                </div>
             </div>
-            <div>
-               <div className={style.content}>
-               </div>
+            <Row>
+               <Col md={12} sm={12} className={style.navitem}>
+                  <div className={style.cardAuthor}>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book1.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book2.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book3.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book4.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                  </div>
+               </Col>
+            </Row>
+            <Row>
+               <Col md={2} sm={2} className={style.navitem}>
+               </Col>
+               <Col md={8} sm={8} className={style.navitem}>
+                  <div className={style.cardAuthor}>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book5.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book6.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book7.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                     <div className={style.card}>
+                        <div className={style.imgBox}>
+                           <Image src="/book8.png" alt="book" width={220} height={300} />
+                        </div>
+                        <div className={style.contentBox}>
+                           <h3>Đắc nhân tâm</h3>
+                           <h2 className={style.price}>61.<small>98</small> €</h2>
+                           <a href="#" className={style.buy}>Buy Now</a>
+                        </div>
+                     </div>
+                  </div>
+               </Col>
+               <Col md={2} sm={2} className={style.navitem}>
+               </Col>
+            </Row>
+
+            <div className={style.pagination}>
+               <a href="#">&laquo;</a>
+               <a href="#">1</a>
+               <a href="#">2</a>
+               <a href="#">3</a>
+               <a href="#">4</a>
+               <a href="#">5</a>
+               <a href="#">6</a>
+               <a href="#">&raquo;</a>
             </div>
-            <div>
-               <div className={style.content}>
-               </div>
-            </div>
-            <div>
-               <div className={style.content}>
-               </div>
-            </div>
-            <div>
-               <div className={style.content}>
-               </div>
-            </div>
-         </div>
-         <div>
-            <div className={style.filter}>
-               <select>
-                  <option value="">Select an author</option>
-                  <option value="1">Tố Hữu</option>
-                  <option value="2">Gegor Canegi</option>
-                  <option value="3">Ông Thọ</option>
-                  <option value="4">Napoleon Hill</option>
-               </select>
-            </div>
-         </div>
-         <div className={style.cardAuthor}>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book1.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book2.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book3.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book4.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-         </div>
-         <div className={style.cardAuthor}>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book5.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book6.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book7.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.imgBox}>
-                  <Image src="/book8.png" alt="book" width={220} height={300} />
-               </div>
-               <div className={style.contentBox}>
-                  <h3>Đắc nhân tâm</h3>
-                  <h2 className={style.price}>61.<small>98</small> €</h2>
-                  <a href="#" className={style.buy}>Buy Now</a>
-               </div>
-            </div>
-         </div>
-         <div className={style.pagination}>
-            <a href="#">&laquo;</a>
-            <a href="#">1</a>
-            <a href="#">2</a>
-            <a href="#">3</a>
-            <a href="#">4</a>
-            <a href="#">5</a>
-            <a href="#">6</a>
-            <a href="#">&raquo;</a>
          </div>
       </div>
 
--- FE/app/blog/page.jsx
+++ FE/app/blog/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import style from "./blog.module.css"
 import { FaAngleRight } from "react-icons/fa6";
-
+import { Row, Col } from 'react-bootstrap';
 
 const BlogPage = () => {
    return (
@@ -14,130 +14,138 @@ const BlogPage = () => {
          </div>
          <Image src="/banner3.jpg" alt="book" width={1740} height={650} />
          <h2>DANH SÁCH BÀI VIẾT</h2>
-         <div className={style.wrapper}>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+         <Row>
+            <Col md={12} sm={12} className={style.navitem}>
+               <div className={style.wrapper}>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
 
 
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
                   </div>
                </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+            </Col>
+         </Row>
+         <Row>
+            <Col md={12} sm={12} className={style.navitem}>
+               <div className={style.wrapper}>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
 
 
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={style.card}>
+                     <div className={style.cardbanner}>
+                        <Image src="/blog1.jpg" alt="book" width={440} height={250} />
+                     </div>
+                     <div className={style.cardbody}>
+                        <p className={style.bloghashtag}>#Food #Pizza</p>
+                        <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
+                        <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
+
+
+                        <div className={style.cardprofileinfo}>
+                           <p className={style.profilefollowers}>5.2k followers</p>
+                        </div>
+                     </div>
                   </div>
                </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div className={style.wrapper}>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-            <div className={style.card}>
-               <div className={style.cardbanner}>
-                  <Image src="/blog1.jpg" alt="book" width={440} height={250} />
-               </div>
-               <div className={style.cardbody}>
-                  <p className={style.bloghashtag}>#Food #Pizza</p>
-                  <h2 className={style.blogtitle}>What is the tastiest pizza in Pizza Hut?</h2>
-                  <p className={style.blogdescription}>My thoughts on the future of front end web development</p>
-
-
-                  <div className={style.cardprofileinfo}>
-                     <p className={style.profilefollowers}>5.2k followers</p>
-                  </div>
-               </div>
-            </div>
-         </div>
+            </Col>
+         </Row>
       </div>
    )
 }
