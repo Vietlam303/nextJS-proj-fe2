d117f4d9f72ed78c25fb56cea4e2293b791fe3b5
Vietlam303 <21211tt1008@mail.tdc.edu.vn>
2024-06-05 00:24:27 +0700

    upd

--- FE/app/review/[slug]/page.jsx
+++ FE/app/review/[slug]/page.jsx
@@ -8,7 +8,8 @@ const singlePost = () => {
         <Container>
             <Row>
                 <div className={style.author}>
-                    <Image></Image>
+                    <Image src="/images/authors/user.png" alt='author' width={30} height={30}></Image>
+                    <span> by Kristian </span>
                 </div>
             </Row>
         <Row>
@@ -22,12 +23,21 @@ const singlePost = () => {
     </div>
         </Col>
         <Col style={{fontSize:'25px'}}>
+        <div className={style.content}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, deleniti est. Sit harum at dolor inventore nulla, iure aliquam repudiandae aperiam, consequuntur hic, doloremque error. Consequuntur, totam quas. Non, culpa.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis doloremque error, sint temporibus ratione deserunt pariatur! Est odio dolorum quos eligendi voluptate. Neque dolor ipsum odio obcaecati eaque maiores consequuntur!loreme
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid facilis quidem dolore veniam error consectetur officiis accusantium aliquam? Deserunt iusto suscipit accusantium voluptate sunt quis impedit labore perferendis iste quam.
+        Incidunt voluptatem quis aperiam provident dolorum voluptate autem at ducimus nihil totam itaque optio quae porro, ratione perspiciatis assumenda! Libero dicta exercitationem mollitia laborum harum consequatur magnam culpa porro dolorum!
+        Fugit natus temporibus repellat ratione dolorem? Ipsam asperiores libero odio autem rerum temporibus cum vero soluta odit. Sunt animi ipsa neque odit autem inventore, sequi doloribus similique repellat impedit voluptatum.
+        Facilis dolor nulla temporibus alias eius laboriosam enim earum voluptatum recusandae ipsam, expedita magni pariatur hic, sapiente ab maxime consectetur! Odit blanditiis quidem at libero veritatis illo veniam eius unde?
+        </div>
         </Col>
     </Row>
     <Row>
+      
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum voluptas aspernatur, nobis assumenda mollitia amet quidem itaque iste debitis ipsum ab voluptates architecto laborum fuga magni, minus sint ipsam nam?
         Hic maiores incidunt ipsa neque fugit iure maxime optio tempore impedit et non eius minima labore velit rem in, eaque nisi, architecto facere necessitatibus, dicta minus quia. Praesentium, similique placeat.
+     
     </Row>
     </Container>
     </div>
